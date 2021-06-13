514fad43cfab1963d4de4633e03ff83e508a3c3a
vishalspoken1 <85843606+vishalspoken1@users.noreply.github.com>
2021-06-13 23:29:18 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -4,6 +4,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		public static void main(String A[])
+		https://github.com/vishallipte/GitDemoProjectV1
+			
+			
 	}
 
 }
