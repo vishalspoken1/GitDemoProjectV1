e8e8e6d373d86aeb710145dfcfa98170244233b0
SAI <SAI@DESKTOP-EVONM5B>
2021-07-02 13:00:10 +0530

    gg

--- src/Test1.java
+++ src/Test1.java
@@ -4,7 +4,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("in main");
-
+		/*Examples:
+			|username			|password	|
+			|test99@gmail.com	|123456		|
+			|test123@gmail.com	|12345      |
+*/
 	}
 
 }
